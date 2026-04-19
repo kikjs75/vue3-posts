aea3dc1c92b4aafd48d6d926bad7d1a889793111
kikjs75 <kikjs75@gmail.com>
2026-04-20 08:46:40 +0900

    vue router

--- src/layouts/TheHeader.vue
+++ src/layouts/TheHeader.vue
@@ -15,12 +15,25 @@
 			</button>
 			<div class="collapse navbar-collapse" id="navbarSupportedContent">
 				<ul class="navbar-nav me-auto">
+					<!-- 변경부분만 리로딩 -->
 					<li class="nav-item">
-						<a class="nav-link active" aria-current="page" href="#">Home</a>
+						<RouterLink class="nav-link" active-class="active" to="/"
+							>Home</RouterLink
+						>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="#">About</a>
+						<RouterLink class="nav-link" active-class="active" to="/about"
+							>About</RouterLink
+						>
 					</li>
+					<!-- 전체페이지 리로딩
+					<li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="/">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="/about">About</a>
+					</li>
+					-->
 				</ul>
 				<form class="d-flex" role="search">
 					<input
---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<h2>Home View</h2>
+		<!-- <p>{{ $route }}</p> -->
+		<p>{{ $route.path }}</p>
+		<p>{{ $route.name }}</p>
+		<button class="btn btn-primary" @click="goAboutPage">
+			About 으로 이동
+		</button>
+	</div>
+</template>
+
+<script setup>
+import router from '@/router';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+console.log('route.path: ', route.path);
+
+const goAboutPage = () => {
+	router.push('/about');
+};
+</script>
+
+<style lang="scss" scoped></style>
