c18767062c81f08625a285b5edfa2a8a931f897b
kikjs75 <kikjs75@gmail.com>
2026-04-21 08:55:16 +0900

    router replace

--- src/views/nested/NestedView.vue
+++ src/views/nested/NestedView.vue
@@ -1,5 +1,6 @@
 <template>
-	<div>
+	<!-- router.push : 히스토리 누적 O -->
+	<!-- <div>
 		<ul class="nav nav-pills">
 			<li class="nav-item">
 				<RouterLink class="nav-link" active-class="active" to="/nested/one"
@@ -8,6 +9,30 @@
 			</li>
 			<li class="nav-item">
 				<RouterLink class="nav-link" active-class="active" to="/nested/two"
+					>Nested Two</RouterLink
+				>
+			</li>
+		</ul>
+		<hr class="my-4" />
+		<RouterView></RouterView>
+	</div> -->
+
+	<!-- router.replace : 히스토리 누적 X -->
+	<div>
+		<ul class="nav nav-pills">
+			<li class="nav-item">
+				<RouterLink
+					class="nav-link"
+					active-class="active"
+					:to="{ name: 'NestedOne', replace: true }"
+					>Nested One</RouterLink
+				>
+			</li>
+			<li class="nav-item">
+				<RouterLink
+					class="nav-link"
+					active-class="active"
+					:to="{ name: 'NestedTwo', replace: true }"
 					>Nested Two</RouterLink
 				>
 			</li>
