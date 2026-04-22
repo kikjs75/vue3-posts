a4eb00eab287df4b52a7d814fb8e023e61bb7711
kikjs75 <kikjs75@gmail.com>
2026-04-22 11:07:21 +0900

    paging, filter

--- src/api/posts.js
+++ src/api/posts.js
@@ -9,8 +9,8 @@ const posts = [
 	{ id: 5, title: '제목5', content: '내용5', createdAt: '2026-05-05' },
 ];
 
-export function getPosts() {
-	return axios.get('http://localhost:5001/posts');
+export function getPosts(params) {
+	return axios.get('http://localhost:5001/posts', { params });
 }
 
 export function getPostById(id) {
--- src/posts/PostListView.vue
+++ src/posts/PostListView.vue
@@ -2,6 +2,22 @@
 	<div>
 		<h2>게시글 목록</h2>
 		<hr class="my-4" />
+		<form @submit.prevent>
+			<div class="row g-3">
+				<div class="col">
+					<input v-model="params.title_like" type="text" class="form-control" />
+				</div>
+				<div class="col-3">
+					<select v-model="params._limit" class="form-select">
+						<option value="3">3개씩 보기</option>
+						<option value="6">6개씩 보기</option>
+						<option value="9">9개씩 보기</option>
+					</select>
+				</div>
+			</div>
+		</form>
+
+		<hr class="my-4" />
 		<div class="row g-3">
 			<div v-for="post in posts" :key="post.id" class="col-4">
 				<PostItem
@@ -12,6 +28,42 @@
 				></PostItem>
 			</div>
 		</div>
+		<nav class="mt-3" aria-label="Page navigation example">
+			<ul class="pagination justify-content-center">
+				<li class="page-item">
+					<a
+						class="page-link"
+						:class="{ disabled: params._page <= 1 }"
+						href="#"
+						aria-label="Previous"
+						@click="--params._page"
+					>
+						<span aria-hidden="true">&laquo;</span>
+					</a>
+				</li>
+				<li
+					v-for="page in pageCount"
+					:key="page"
+					class="page-item"
+					:class="{ active: params._page === page }"
+				>
+					<a class="page-link" href="#" @click="params._page = page">{{
+						page
+					}}</a>
+				</li>
+				<li class="page-item">
+					<a
+						class="page-link"
+						:class="{ disabled: params._page >= pageCount }"
+						href="#"
+						aria-label="Next"
+						@click="++params._page"
+					>
+						<span aria-hidden="true">&raquo;</span>
+					</a>
+				</li>
+			</ul>
+		</nav>
 		<hr class="my-4" />
 		<AppCard>
 			<PostDetailView :id="2"></PostDetailView>
@@ -22,13 +74,29 @@
 <script setup>
 import { getPosts } from '@/api/posts';
 import PostItem from '@/components/posts/PostItem.vue';
-import { ref } from 'vue';
+import { computed, ref, watchEffect } from 'vue';
 import { useRouter } from 'vue-router';
 import PostDetailView from './PostDetailView.vue';
 import AppCard from '@/components/AppCard.vue';
 
 const router = useRouter();
 const posts = ref([]);
+const params = ref({
+	_sort: 'createdAt',
+	_order: 'desc',
+	_page: 1,
+	_limit: 3,
+	title_like: '',
+});
+const totalCount = ref(0);
+// 1) computed(return 필요)
+// const pageCount = computed(() => {
+// 	return  Math.ceil(totalCount.value / params.value._limit);
+// });
+// 2) computed(return 불필요)
+const pageCount = computed(() =>
+	Math.ceil(totalCount.value / params.value._limit),
+);
 
 // const fetchPosts = () => {
 // 	posts.value = getPosts();
@@ -67,13 +135,15 @@ const posts = ref([]);
 
 const fetchPosts = async () => {
 	try {
-		const { data } = await getPosts(); // 구조분해로 받는다.
+		const { data, headers } = await getPosts(params.value); // 구조분해로 받는다.
 		posts.value = data;
+		totalCount.value = headers['x-total-count']; // 소문자.
 	} catch (error) {
 		console.log(error);
 	}
 };
-fetchPosts();
+// fetchPosts();
+watchEffect(fetchPosts); // Watching 함수의 반응형 데이터가 바뀌어서 다시 실행된다.
 
 // const fetchPosts = async () => {
 // 	({ data: posts.value } = await getPosts()); // 구조분해로 이렇게도 받을 수 있다.
